8699efe046df2ca74cae21dd6d0d4cb9a7460b37
srinivassriram <Cheenu06!>
2020-08-26 16:29:54 -0400

    e

--- Mask_Detector/play_audioMask.py
+++ Mask_Detector/play_audioMask.py
@@ -2,13 +2,7 @@
 from constants import sound_file
 import subprocess
 import os
-from logger import Logger
 
-# This file implements logic to play a speech audio.
-from constants import sound_file
-import subprocess
-import os
-from logger import Logger
 
 
 class PlayAudio:
@@ -25,40 +19,15 @@ class PlayAudio:
         play_audio_successful = False
         try:
             speech_file_path = os.path.join(os.path.dirname(__file__), sound_file)
-            Logger.logger().info("Trying to open {}.".format(speech_file_path))
+            print("Trying to open {}.".format(speech_file_path))
             return_code = subprocess.call(["aplay", speech_file_path])
             play_audio_successful = True
         except KeyboardInterrupt:
-            Logger.logger().info('\nInterrupted by user')
+            print('\nInterrupted by user')
         except Exception as e:
-            Logger.logger().info(type(e).__name__ + ': ' + str(e))
+            print(type(e).__name__ + ': ' + str(e))
         else:
-            Logger.logger().info("Played {} with return code {}.".format(sound_file, return_code))
+            print("Played {} with return code {}.".format(sound_file, return_code))
         finally:
             return play_audio_successful
 
-class PlayAudio:
-    """
-    This class implements the logic to play audio files.
-    """
-
-    @classmethod
-    def play_audio_file(cls):
-        """
-        This method implements logic to play audio file.
-        :return:
-        """
-        play_audio_successful = False
-        try:
-            speech_file_path = os.path.join(os.path.dirname(__file__), sound_file)
-            Logger.logger().info("Trying to open {}.".format(speech_file_path))
-            return_code = subprocess.call(["aplay", speech_file_path])
-            play_audio_successful = True
-        except KeyboardInterrupt:
-            Logger.logger().info('\nInterrupted by user')
-        except Exception as e:
-            Logger.logger().info(type(e).__name__ + ': ' + str(e))
-        else:
-            Logger.logger().info("Played {} with return code {}.".format(sound_file, return_code))
-        finally:
-            return play_audio_successful
