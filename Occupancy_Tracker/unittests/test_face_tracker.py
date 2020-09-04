6b08163a6ce2151c5bcbd8a2626b7156eab6e669
Aditya <adityaanand.muz@gmail.com>
2020-09-04 12:41:25 -0400

    Did the test_one_by_two_cross_EnterExit and it passed

--- Occupancy_Tracker/unittests/test_face_tracker.py
+++ Occupancy_Tracker/unittests/test_face_tracker.py
@@ -239,5 +239,24 @@ class TestFaceTracker(unittest.TestCase):
         human_detector_inst.clean_up()
         self.__cleanup()
 
+    def test_one_by_two_cross_EnterExit(self):
+        """
+        This method validates if occupancy detector can actually detect 1 person entering and teh other exiting the premises.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/TempleVideos/one_by_two_cross_Enter_Exit.mp4',
+            use_pi_camera=False, open_display=True)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 3)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[2].direction, Direction.ENTER)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), -1)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+
 if __name__ == '__main__':
     unittest.main()
