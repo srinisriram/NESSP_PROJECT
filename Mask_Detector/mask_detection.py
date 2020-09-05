80d9e951d9327d0ff11185ce5ed0c8d41f61c40d
srinivassriram <Cheenu06!>
2020-09-05 16:56:40 -0400

    Updated latest code

--- Mask_Detector/mask_detection.py
+++ Mask_Detector/mask_detection.py
@@ -58,13 +58,27 @@ class MaskDetector:
 
     @classmethod
     def perform_job(cls, preferableTarget=cv2.dnn.DNN_TARGET_CPU):
+        """
+        This method performs the job expected from this class.
+        :key
+        """
+        # Set preferable target.
         MaskDetector.preferable_target = preferableTarget
+        # Set input video file path (if applicable)
         MaskDetector.input_video_file_path = MaskDetector.input_video_file_path
+        # Create a thread that uses the thread_for_mask_detection function and start it.
         t1 = threading.Thread(target=MaskDetector().thread_for_mask_detection)
         t1.start()
 
     def load_caffe_model(self):
+        """
+        This function will load the caffe model that we will use for detecting a face, and then set the preferable target to the correct target.
+        :key
+
+        """
         print("Loading caffe model used for detecting a face.")
+
+        # Use cv2.dnn function to read the caffe model used for detecting faces and set preferable target.
         self.detector = cv2.dnn.readNetFromCaffe(os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             prototxt_path),
@@ -75,7 +89,14 @@ class MaskDetector:
         self.detector.setPreferableTarget(MaskDetector.preferable_target)
 
     def load_pytorch_model(self):
+        """
+        This function will load the pytorch model that is used for extracting the facial embeddings, and then we set the correct preferable target.
+
+        :key
+        """
         print("Loading pytorch embedding model used for extracting the facial embeddings.")
+
+        # Load pytorch model used for extracting embeddings and set preferable target.
         self.embedder = cv2.dnn.readNetFromTorch(os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             embedder_path))
@@ -83,8 +104,15 @@ class MaskDetector:
         self.embedder.setPreferableTarget(MaskDetector.preferable_target)
 
     def load_pickle_recognizer(self):
+        """
+        This function will load our 2 pickle files. One file contains our SVM Machine trained in scikit-learn to classify the embeddings (recognizer.pickle).
+        Another file contains the Label Encoder that will encode the output that our SVM model provides as a label, with or without mask.
+
+        :key
+        """
         print("Loading pickle files that are used to determine the class of the embeddings.")
 
+        # Load pickle files.
         self.recognizer = pickle.loads(open(os.path.join(
             os.path.dirname(os.path.realpath(__file__)),
             recognizer_path), "rb").read())
@@ -94,6 +122,10 @@ class MaskDetector:
             labels_path), "rb").read())
 
     def initialize_camera(self):
+        """
+        This function will initialize the camera or video stream by figuring out whether to stream the camera capture or from a video file.
+        :key
+        """
         if MaskDetector.input_video_file_path is None:
             print("[INFO] starting video stream...")
             self.vs = VideoStream(src=0).start()
@@ -102,51 +134,99 @@ class MaskDetector:
             self.vs = cv2.VideoCapture(MaskDetector.input_video_file_path)
 
     def grab_next_frame(self):
+        """
+        This function extracts the next frame from the video stream.
+
+        :return:
+        """
         if MaskDetector.input_video_file_path is None:
             self.frame = self.vs.read()
         else:
             _, self.frame = self.vs.read()
-            #self.frame = cv2.rotate(self.frame, cv2.ROTATE_180)
+            # self.frame = cv2.rotate(self.frame, cv2.ROTATE_180)
 
         if self.frame is None:
             return
-
         self.frame = imutils.resize(self.frame, width=frame_width_in_pixels)
 
-    def set_dimensions_for_face_blob(self):
+    def set_dimensions_for_frame(self):
+        """
+        This function will set the frame dimensions, which we will use later on.
+        :key
+        """
         if not self.h or not self.w:
+            #Set frame dimensions.
             (self.h, self.w) = self.frame.shape[:2]
 
-    def create_face_blob(self):
+    def create_frame_blob(self):
+        """
+        This function will create a blob for our face detector to detect a face.
+        :key
+        """
         self.image_blob = cv2.dnn.blobFromImage(
             cv2.resize(self.frame, (300, 300)), 1.0, (300, 300),
             (104.0, 177.0, 123.0), swapRB=False, crop=False)
 
     def extract_face_detections(self):
+        """
+        This function will extract each face detection that our face detection model provides.
+        :return:
+        """
         self.detector.setInput(self.image_blob)
         self.detections = self.detector.forward()
 
     def extract_confidence_from_face_detections(self, i):
+        """
+        This function will extract the confidence(probability) of the face detection so that we can filter out weak detections.
+        :param i:
+        :return:
+        """
+
         self.confidence = self.detections[0, 0, i, 2]
 
     def create_face_box(self, i):
+        """
+        This function will define coordinates of the face.
+        :param i:
+        :return:
+        """
         self.box = self.detections[0, 0, i, 3:7] * np.array([self.w, self.h, self.w, self.h])
         (self.startX, self.startY, self.endX, self.endY) = self.box.astype("int")
 
     def extract_face_roi(self):
+        """
+        This function will use the coordinates defined earlier and create a ROI that we will use for embeddings.
+        :return:
+        """
         self.face = self.frame[self.startY:self.endY, self.startX:self.endX]
         (self.f_h, self.f_w) = self.face.shape[:2]
 
     def create_embeddings_blob(self):
+        """
+        This function will create another blob out of the face ROI that we will use for extracting the embeddings.
+        :return:
+        """
         self.face_blob = cv2.dnn.blobFromImage(cv2.resize(self.face,
                                                           (96, 96)), 1.0 / 255, (96, 96), (0, 0, 0),
                                                swapRB=True, crop=False)
 
     def extract_embeddings_detections(self):
+        """
+        This function extracts the embeddings from the face detections.
+        :return:
+        """
         self.embedder.setInput(self.face_blob)
         self.vec = self.embedder.forward()
 
     def perform_classification(self):
+        """
+        This function will now use the pickle files to do the following:
+            1. Extract the class prediction of the embeddings.
+            2. Get the probability of the prediction.
+            3. Get the label of the prediction.
+
+        :return:
+        """
         self.predictions = self.recognizer.predict_proba(self.vec)[0]
         self.j = np.argmax(self.predictions)
         self.probability = self.predictions[self.j]
@@ -154,6 +234,7 @@ class MaskDetector:
 
     def create_frame_icons(self):
         """
+        This function will create the icons that will be displayed on the frame.
 
         :return:
         """
@@ -162,6 +243,10 @@ class MaskDetector:
         self.colorIndex = LABELS.index(self.name)
 
     def play_audio(self):
+        """
+        This function is used for playing the alarm if a person is not wearing a mask.
+        :return:
+        """
         SoundThread = threading.Thread(target=PlayAudio.play_audio_file)
         print("[INFO]: Starting Sound Thread")
         if not self.AudioPlay:
@@ -172,10 +257,14 @@ class MaskDetector:
             print("[INFO]: Stopping Sound Thread")
 
     def loop_over_frames(self):
+        """
+        This is the main function that will loop through the frames and use the functions defined above to detect for face mask.
+        :return:
+        """
         while MaskDetector.run_program:
             self.grab_next_frame()
-            self.set_dimensions_for_face_blob()
-            self.create_face_blob()
+            self.set_dimensions_for_frame()
+            self.create_frame_blob()
             self.extract_face_detections()
 
             for i in range(0, self.detections.shape[2]):
@@ -208,10 +297,18 @@ class MaskDetector:
                     break
 
     def clean_up(self):
+        """
+        Clean up the cv2 video capture.
+        :return:
+        """
         cv2.destroyAllWindows()
         self.vs.release()
 
     def thread_for_mask_detection(self):
+        """
+        Callable function that will run the mask detector and can be invoked in a thread.
+        :return:
+        """
         while MaskDetector.run_program:
             try:
                 self.loop_over_frames()
--- Occupancy_Tracker/unittests/test_face_tracker.py
+++ Occupancy_Tracker/unittests/test_face_tracker.py
@@ -331,5 +331,167 @@ class TestFaceTracker(unittest.TestCase):
         human_detector_inst.clean_up()
         self.__cleanup()
 
+    def test_occupancy_1(self):
+        """
+        This method validates if occupancy detector can actually correctly calculate the number of people entering the premises on a typical temple day.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/occupancy_test_videos/occupancy1.mp4',
+            use_pi_camera=False, open_display=False)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 9)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[2].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[3].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[4].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[5].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[6].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[7].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[8].direction, Direction.ENTER)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), 3)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+    def test_occupancy_3(self):
+        """
+        This method validates if occupancy detector can actually correctly calculate the number of people entering the premises on a typical temple day.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/occupancy_test_videos/occupancy3.mp4',
+            use_pi_camera=False, open_display=False)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 5)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[2].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[3].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[4].direction, Direction.ENTER)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), 5)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+    def test_occupancy_5(self):
+        """
+        This method validates if occupancy detector can actually correctly calculate the number of people entering the premises on a typical temple day.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/occupancy_test_videos/occupancy5.mp4',
+            use_pi_camera=False, open_display=False)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 9)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[2].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[3].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[4].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[5].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[6].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[7].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[8].direction, Direction.ENTER)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), 7)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+    def test_occupancy_6(self):
+        """
+        This method validates if occupancy detector can actually correctly calculate the number of people entering the premises on a typical temple day.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/occupancy_test_videos/occupancy6.mp4',
+            use_pi_camera=False, open_display=False)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 12)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[2].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[3].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[4].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[5].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[6].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[7].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[8].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[9].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[10].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[11].direction, Direction.ENTER)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), 2)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+    def test_occupancy_7(self):
+        """
+        This method validates if occupancy detector can actually correctly calculate the number of people entering the premises on a typical temple day.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/occupancy_test_videos/occupancy7.mp4',
+            use_pi_camera=False, open_display=False)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 2)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.EXIT)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), 0)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+    def test_occupancy_8(self):
+        """
+        This method validates if occupancy detector can actually correctly calculate the number of people entering the premises on a typical temple day.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/occupancy_test_videos/occupancy8.mp4',
+            use_pi_camera=False, open_display=False)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 8)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[2].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[3].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[4].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[5].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[6].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[7].direction, Direction.ENTER)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), 6)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+    def test_occupancy_9(self):
+        """
+        This method validates if occupancy detector can actually correctly calculate the number of people entering the premises on a typical temple day.
+        :return:
+        """
+        human_detector_inst = HumanDetector(
+            find_humans_from_video_file_name='videos/occupancy_test_videos/occupancy9.mp4',
+            use_pi_camera=False, open_display=False)
+        self.assertEqual(human_detector_inst.perform_job(), None)
+        human_centroid_dict = human_detector_inst.get_human_centroid_dict()
+        self.assertEqual(len(human_centroid_dict), 11)
+        self.assertEqual(human_centroid_dict[0].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[1].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[2].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[3].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[4].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[5].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[6].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[7].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[8].direction, Direction.ENTER)
+        self.assertEqual(human_centroid_dict[9].direction, Direction.EXIT)
+        self.assertEqual(human_centroid_dict[10].direction, Direction.ENTER)
+        self.assertEqual(SendReceiveMessages().get_face_detected_count_locally(), 3)
+        human_detector_inst.clean_up()
+        self.__cleanup()
+
+
 if __name__ == '__main__':
     unittest.main()
