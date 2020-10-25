7e05a0ed57292630f5ba4b6a12f0638cc658de0d
Srinivas Sriram <srinivassriram06@gmail.com>
2020-10-25 10:27:57 -0400

    Added crash sending functionality to mask detector

--- etc/crash_report.py
+++ etc/crash_report.py
@@ -5,8 +5,7 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.utils import formatdate
-
-from Occupancy_Tracker.constants import OCCUPATION_LOG
+import argparse
 
 
 class CrashReport:
@@ -14,11 +13,22 @@ class CrashReport:
         This class composes and sends an email when the occupancy crashes.
     """
 
-    @classmethod
-    def email_send(cls, occupation_log=OCCUPATION_LOG):
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-l", "--log_file", type=str,
+                            help="Provide the log file to send via email", required=True)
+        parser.add_argument('-o', '--occupancy', type=bool,
+                            help='Required to send occupancy crash email', default=False)
+        parser.add_argument('-m', '--mask', type=str,
+                            help='Required to send mask crash email', default=False)
+        self.args = parser.parse_args()
+
+    def perform_job(self):
+        self.email_send()
+
+    def email_send(self):
         """
-        This method sends an email 
-        :param occupation_log: 
+        This method sends an email
         :return: 
         """
         msg = MIMEMultipart()
@@ -28,14 +38,17 @@ class CrashReport:
         msg['From'] = 'maskdetector101@gmail.com'
         msg['To'] = "adityaanand.muz@gmail.com, srinivassriram06@gmail.com, raja.muz@gmail.com, abhisar.muz@gmail.com"
         msg['Date'] = formatdate(localtime=True)
-        msg['Subject'] = 'The Occupancy Tracker has crashed!'
-
-        body = "The Occupancy Tracker has crashed and the occupation log is attached below."
+        if self.args.occupancy:
+            msg['Subject'] = 'The Occupancy Tracker has crashed!'
+            body = "The Occupancy Tracker has crashed and the occupation log is attached below."
+        elif self.args.mask:
+            msg['Subject'] = 'The Mask Detector has crashed!'
+            body = "The Mask Detector has crashed and the mask log is attached below"
 
         part = MIMEBase('application', "octet-stream")
-        part.set_payload(open(occupation_log, "rb").read())
+        part.set_payload(open(self.args.log_file, "rb").read())
         encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment', filename=occupation_log)
+        part.add_header('Content-Disposition', 'attachment', filename=self.args.log_file)
         msg.attach(part)
 
         msg.attach(MIMEText(body, "plain"))
@@ -53,4 +66,4 @@ class CrashReport:
 
 
 if __name__ == '__main__':
-    CrashReport.email_send()
+    CrashReport().perform_job()
--- etc/on_reboot_occupancy.sh
+++ etc/on_reboot_occupancy.sh
@@ -4,11 +4,11 @@ source `which virtualenvwrapper.sh`
 workon openvino
 source openvino/openvino/bin/setupvars.sh
 export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT
-cd NESSP_PROJECT/Occupancy_Tracker
 rm -rf Occupation.log
+cd NESSP_PROJECT/Occupancy_Tracker
 python3 human_detector.py -i 192.168.86.73 &>> /home/pi/Occupation.log
 echo "script exiting..." &>> /home/pi/Occupation.log
 echo "sending crash report via email..." &>> /home/pi/Occupation.log
-python3 crash_report.py
+python3 crash_report.py -l /home/pi/Occupation.log -o True
 echo "rebooting..." &>> /home/pi/Occupation.log
 sudo reboot
