884a79ff5db4594609e34834605538021b57d1d0
srinisriram <srinivassriram06@gmail.com>
2020-10-25 12:26:30 -0400

    Added new shell scripts

--- etc/on_reboot_mask.sh
+++ etc/on_reboot_mask.sh
@@ -2,13 +2,13 @@
 
 source `which virtualenvwrapper.sh`
 workon openvino
-source openvino/openvino/bin/setupvars.sh
+source ~/openvino/bin/setupvars.sh
 export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT
 rm -rf Mask.log
-cd NESSP_PROJECT/Mask_Detector
+cd ~/NESSP_PROJECT/Mask_Detector
 python3 mask_detector.py &>> /home/pi/Mask.log
 echo "script exiting..." &>> /home/pi/Mask.log
 echo "sending crash report via email..." &>> /home/pi/Mask.log
-python3 crash_report.py -l /home/pi/Mask.log -m True
-echo "rebooting..." &>> /home/pi/Mask.log
-sudo reboot
+python3 ~/NESSP_PROJECT/etc/crash_report.py -l /home/pi/Mask.log -m True
+#echo "rebooting..." &>> /home/pi/Mask.log
+#sudo reboot
--- etc/on_reboot_occupancy.sh
+++ etc/on_reboot_occupancy.sh
@@ -2,13 +2,13 @@
 
 source `which virtualenvwrapper.sh`
 workon openvino
-source openvino/openvino/bin/setupvars.sh
+source ~/openvino/bin/setupvars.sh
 export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT
 rm -rf Occupation.log
-cd NESSP_PROJECT/Occupancy_Tracker
+cd ~/NESSP_PROJECT/Occupancy_Tracker
 python3 human_detector.py -i 192.168.86.73 &>> /home/pi/Occupation.log
 echo "script exiting..." &>> /home/pi/Occupation.log
 echo "sending crash report via email..." &>> /home/pi/Occupation.log
-python3 crash_report.py -l /home/pi/Occupation.log -o True
-echo "rebooting..." &>> /home/pi/Occupation.log
-sudo reboot
+python3 ~/NESSP_PROJECT/etc/crash_report.py -l /home/pi/Occupation.log -o True
+#echo "rebooting..." &>> /home/pi/Occupation.log
+#sudo reboot
