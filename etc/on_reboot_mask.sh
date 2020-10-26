d22462c90be1f661a83d4b62de6c7dccbe1e734a
abhisaranand <abhisar.muz@gmail.com>
2020-10-26 08:57:24 -0400

    Tweaked the sh files as discussed.

--- etc/on_reboot_mask.sh
+++ etc/on_reboot_mask.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+echo "Initializing Environment..." &> /home/pi/Mask.log
 source `which virtualenvwrapper.sh`
 workon openvino
 source ~/openvino/bin/setupvars.sh
 export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT
-rm -rf Mask.log
 cd ~/NESSP_PROJECT/Mask_Detector
+echo "Running the Script..." &>> /home/pi/Mask.log
 python3 mask_detector.py &>> /home/pi/Mask.log
-echo "script exiting..." &>> /home/pi/Mask.log
-echo "sending crash report via email..." &>> /home/pi/Mask.log
+echo "Script Exiting..." &>> /home/pi/Mask.log
+echo "Sending Crash Report via Email..." &>> /home/pi/Mask.log
 python3 ~/NESSP_PROJECT/etc/crash_report.py -l /home/pi/Mask.log -m True
+echo "Crash Report Sent..." &>> /home/pi/Mask.log
 #echo "rebooting..." &>> /home/pi/Mask.log
 #sudo reboot
--- etc/on_reboot_occupancy.sh
+++ etc/on_reboot_occupancy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+echo "Initializing Environment..." &> Occupation.log
 source `which virtualenvwrapper.sh`
 workon openvino
 source ~/openvino/bin/setupvars.sh
 export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT
-echo "Testing Audio..."
+echo "Testing Audio..." &>> Occupation.log
 aplay test_audio.wav
-rm -rf Occupation.log
 cd ~/NESSP_PROJECT/Occupancy_Tracker
+echo "Running the Script..." &>> Occupation.log
 python3 human_detector.py -i 192.168.86.73 &>> /home/pi/Occupation.log
-echo "script exiting..." &>> /home/pi/Occupation.log
-echo "sending crash report via email..." &>> /home/pi/Occupation.log
+echo "Script Exiting..." &>> /home/pi/Occupation.log
+echo "Sending Crash Report Via Email..." &>> /home/pi/Occupation.log
 python3 ~/NESSP_PROJECT/etc/crash_report.py -l /home/pi/Occupation.log -o True
+echo "Crash Report Sent..." &>> Occupation.log
 #echo "rebooting..." &>> /home/pi/Occupation.log
 #sudo reboot
