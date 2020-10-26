dc055eafa4e33616986d89db46edb350726bce00
abhisaranand <abhisar.muz@gmail.com>
2020-10-26 09:48:33 -0400

    Fixed some bugs in the sh script.

--- etc/on_reboot_occupancy.sh
+++ etc/on_reboot_occupancy.sh
@@ -3,10 +3,10 @@
 echo "Initializing Environment..." &> Occupation.log
 source `which virtualenvwrapper.sh`
 workon openvino
-source ~/openvino/bin/setupvars.sh
+source ~/openvino/openvino/bin/setupvars.sh
 export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT
-echo "Testing Audio..." &>> Occupation.log
-aplay test_audio.wav
+#echo "Testing Audio..." &>> Occupation.log
+#aplay test_audio.wav
 cd ~/NESSP_PROJECT/Occupancy_Tracker
 echo "Running the Script..." &>> Occupation.log
 python3 human_detector.py -i 192.168.86.73 &>> /home/pi/Occupation.log
