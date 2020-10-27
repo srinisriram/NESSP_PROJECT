e48e07c2ef4647719073619f93e556073a318df7
abhisaranand <abhisar.muz@gmail.com>
2020-10-27 09:46:18 -0400

    did stuff

--- etc/on_reboot_occupancy.sh
+++ etc/on_reboot_occupancy.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-echo "Initializing Environment..." &>Occupation.log
+echo "Initializing Environment..." &>/home/pi/Occupation.log
 # shellcheck disable=SC1090
 # shellcheck disable=SC2046
 # shellcheck disable=SC2230
-source $(which virtualenvwrapper.sh)
-workon openvino
+# shellcheck disable=SC2129
+source $(which virtualenvwrapper.sh) &>>/home/pi/Occupation.log
+workon openvino &>>/home/pi/Occupation.log
 # shellcheck disable=SC1090
-source ~/openvino/openvino/bin/setupvars.sh
-export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT
+source ~/openvino/openvino/bin/setupvars.sh &>>/home/pi/Occupation.log
+export PYTHONPATH=$PYTHONPATH:/home/pi/NESSP_PROJECT &>>/home/pi/Occupation.log
 #echo "Testing Audio..." &>> Occupation.log
 #aplay test_audio.wav
 # shellcheck disable=SC2164
 cd ~/NESSP_PROJECT/Occupancy_Tracker
-echo "Running the Script..." &>>Occupation.log
+# shellcheck disable=SC2129
+echo "Running the Script..." &>>/home/pi/Occupation.log
 # shellcheck disable=SC2129
 python3 human_detector.py -i 192.168.86.73 &>>/home/pi/Occupation.log
 echo "Script Exiting..." &>>/home/pi/Occupation.log
 echo "Sending Crash Report Via Email..." &>>/home/pi/Occupation.log
-python3 ~/NESSP_PROJECT/etc/crash_report.py -l /home/pi/Occupation.log -o True
-echo "Crash Report Sent..." &>>Occupation.log
+python3 ~/NESSP_PROJECT/etc/crash_report.py -l /home/pi/Occupation.log -o True &>>/home/pi/Occupation.log
+echo "Crash Report Sent..." &>>/home/pi/Occupation.log
 #echo "rebooting..." &>> /home/pi/Occupation.log
 #sudo reboot
